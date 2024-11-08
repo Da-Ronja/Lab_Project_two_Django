41659c61b9335c3e3ab188fc088bdc6e99a36caa
Silvia <trollunge86@gmail.com>
2024-11-08 16:02:03 +0100

    lab 2 add template help page

---
+++ AppTwo/urls.py
@@ -0,0 +1,9 @@
+# Project_two/AppTwo/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'), 
+    path('help/', views.help, name='help'), 
+]
--- AppTwo/views.py
+++ AppTwo/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<em>My Second App</em>")
+
+def help(request):
+    return render(request, 'help.html')
